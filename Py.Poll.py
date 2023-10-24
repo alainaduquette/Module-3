3420e50fc21460a43d7651203723068b2fc70e9e
alainaduquette <145624399+alainaduquette@users.noreply.github.com>
2023-10-23 20:43:10 -0400

    Add files via upload

--- Py.Poll.py
+++ Py.Poll.py
@@ -1,62 +1,38 @@
 import csv
-
-#open the csv
-with open("C:\\Users\\alain\\Documents\\Bootcamp\\Python Challenge\\Python-Challenge\\PyRoll\\Resources\\election_data.csv", "r") as file:
-    #read the first line
-    header=file.readline()
-    #set the name of my readr 
+#open CSV
+with open("C:\\Users\\alain\\Documents\\Bootcamp\\Python Challenge\\Python-Challenge\\PyBank\\Resources\\budget_data.csv", "r") as file:
+    #set header and tell system to read
+    header=file.readline() 
+    #set the name of the reader
     my_reader = csv.reader(file, delimiter=",")
-    #create a dictionary to begin adding to
-    candidate_dictionary = {}
+    #set the below variables to "0" to eventually have them add up
+    total = 0
+    month_counter = 0
+    sum = 0
+    #create a blank dictionary to add the data to
+    changedict={}
     
-    #create a list that allows to later turn into a count of total
-    ballot_id = []
+    #set a for loop to go through every row of data. The first if statement is saying if this is the first month set the temp to the pofit
+    for date,profit in my_reader:
+        if (month_counter == 0):
+            temp = int(profit)
+       #the else will complete the rest of the for loops through the rows to generate the rest of the dictionary 
+        else:
+            changedict[date] = int(profit) - temp
+            temp = int(profit)
+        #adding a tick to the month counter
+        month_counter = month_counter + 1
 
-    #create a for loop that runs through all lines in the csv
-    for row in my_reader:
-        #states the column which cadidate name is 
-        candidate_name = row[2]
-        #states if the candidate is not already in the dictionary add the candidate and set the total count along with it to 0
-        if candidate_name not in candidate_dictionary.keys():
-            candidate_dictionary[candidate_name] = 0
-         
-         #if the candidate name is in the dictionary than add 1 to the count
-        candidate_dictionary[candidate_name] = candidate_dictionary[candidate_name] +1 
-        #adding to the dic to count total
-        ballot_id.append({'county':row[1], 'candidate':row[2]})
-        
-count = len(ballot_id)
+        total = total + int(profit)
 
-#create a new list for the percent
-total_percent = []
-#create a for loop to go through the csv to then add the percents
-for votes_cast in candidate_dictionary.values():
-    total_percent.append(round(((votes_cast / count)*100), 3))
-#
-total_percent_index=0
-
-print("Election Resuluts")
-print("------------------------------------------------")
-print("Total Votes: " + str(count))
-print("------------------------------------------------")
-for candidate_name, votes_cast in candidate_dictionary.items():
-   
-
-    print(candidate_name +": " + str(total_percent[total_percent_index]) + "% " +"("+ str(votes_cast) +")")
-    total_percent_index=total_percent_index+1
-
-print("------------------------------------------------")
-
-print("Winner: " + max(candidate_dictionary, key=candidate_dictionary.get), max(candidate_dictionary.values()))
-print("------------------------------------------------")
-
-
-
-
-
-
-    
-
-
-
+#the below is to be printed out
+print("Financial Analysis")
+print("")
+print("----------------------------")
+print("")
+print("Total Months: " + str(month_counter))
+print("Total: " + str(total))
+print("Average Change: " + str(int(total) / int(len(changedict))))
+print("Greatest Increase in Profits: " + max(changedict, key=changedict.get), max(changedict.values()))
+print("Greatest Decrease in Profits: " + min(changedict, key=changedict.get), max(changedict.values()))
 
